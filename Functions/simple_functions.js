c4552ae6bd0e94ae79b3a5bc1c0b924c8a6f1151
Zion Danielson <zionsdocs@gmail.com>
2023-05-02 11:39:03 -0400

    simple_functions.js

---
+++ Functions/simple_functions.js
@@ -0,0 +1,39 @@
+"use script";
+
+// 1.
+function favoriteThings(){
+    let name = ("Zion");
+    let favMovie = ("SeventeenAgain");
+    let favMusician = ("JanetJackson");
+
+     console.log("My name is " + name + ". " 
+    + "My favorite movie right now is " + favMovie + ". " 
+    + "My favorite musician is " + favMusician + ".");
+}
+
+// 2.
+function whyImHere(){
+    let reason = ("I want to further my education, build a new skill and put my self inn the position to pursue a career.")
+
+    console.log("I am here at Year up because, " + reason)
+}
+
+// 3.
+function favoritePlace(){
+    let favoritePlace = ("the beach")
+
+    console.log("One of my favorite places to go to is " + favoritePlace + ".")
+}
+
+// Calling each of my functions
+
+console.log("Zion's favorite things:")
+favoriteThings()
+
+console.log("Zion's Why I'm Here:")
+whyImHere()
+
+console.log("Zion's favorite Place:")
+favoritePlace()
+
+
